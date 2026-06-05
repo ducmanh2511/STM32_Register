8d3528fd662e75293b72fe591161a575d246276a
Lê Đức Mạnh <manh.ld25112005@gmail.com>
2026-06-05 23:45:56 +0700

    Update NVIC for EXTI driver

--- Driver/EXTI.c
+++ Driver/EXTI.c
@@ -5,7 +5,6 @@ void EXTI_EnableInterrupt(uint8_t line){
 	}
 	else{
 		EXTI->EXTI_IMR|= (1<<line);
-		
 	}
 
 }
@@ -18,25 +17,40 @@ void EXTI_DisableInterrupt(uint8_t line){
 		
 	}
 }
-void EXTI_SetMode(uint8_t line, EXTI_MODE mode){
-	EXTI->EXTI_RTSR &= ~(1U<<line); // reset 
-	EXTI->EXTI_FTSR &= ~(1U<<line);   // reset
+void EXTI_Init(GPIO_Typedef *port, uint8_t pin, EXTI_MODE mode){
+	if(port==GPIOA){
+		AFIO_SetEXTI_Port(AFIO_EXTI_PA,pin);
+		
+	}
+	else if(port==GPIOB){
+		AFIO_SetEXTI_Port(AFIO_EXTI_PB,pin);
+	}
+	else if(port==GPIOC){
+		AFIO_SetEXTI_Port(AFIO_EXTI_PC,pin);
+	}
+	else if(port==GPIOD){
+		AFIO_SetEXTI_Port(AFIO_EXTI_PD,pin);
+	}
+	
+	EXTI->EXTI_RTSR &= ~(1U<<pin); // reset 
+	EXTI->EXTI_FTSR &= ~(1U<<pin);   // reset
 	switch (mode){
 		case EXTI_RISING :
-			EXTI->EXTI_RTSR |= (1U<<line);
+			EXTI->EXTI_RTSR |= (1U<<pin);
 			break;
 		
 	 	case EXTI_FALLING :
-			EXTI->EXTI_FTSR |= (1U<<line);
+			EXTI->EXTI_FTSR |= (1U<<pin);
 	  	break;
 		case EXTI_BOTH :
-			EXTI->EXTI_RTSR |= (1U<<line);
-      EXTI->EXTI_FTSR |= (1U<<line);
+			EXTI->EXTI_RTSR |= (1U<<pin);
+      EXTI->EXTI_FTSR |= (1U<<pin);
 			break;
 		default :
 			break;
 		
 	}
+	EXTI_EnableInterrupt(pin);
 }
 uint8_t EXTI_GetPending(uint8_t line)
 {
@@ -46,6 +60,28 @@ uint8_t EXTI_GetPending(uint8_t line)
 }
 void EXTI_ClearPending(uint8_t line){
     EXTI->EXTI_PR = (1U << line);
+}
+
+
+void NVIC_EXTI0_Enable(void){
+	NVIC->NVIC_ISER0 |= 1U<<6;
+	
+}
+void NVIC_EXTI1_Enable(void){
+   NVIC->NVIC_ISER0 |= 1U<<7;}
+void NVIC_EXTI2_Enable(void){
+	NVIC->NVIC_ISER0 |= 1U<<8;
+}
+void NVIC_UART1_Enable(void){
+   NVIC->NVIC_ISER1 |= 1<<5;
+  
+}
+void EXTI0_IRQHandler(void){ 
+	   if(EXTI_GetPending(0)){
+			 // do something
+			 EXTI_ClearPending(0);
+		 }
+	
 }
 
 
--- Driver/EXTI.h
+++ Driver/EXTI.h
@@ -2,7 +2,10 @@
 #define EXTI_H
 #include <stdio.h>
 #include <stdint.h>
+#include <GPIO.h>
+#include <AFIO.h>
 #define EXTI_ADD_BASE         0x40010400
+#define NVIC_ADD_BASE         0xE000E100
 typedef enum {
 	  EXTI_RISING,
 	  EXTI_FALLING,
@@ -17,14 +20,31 @@ typedef struct{
 	 volatile uint32_t EXTI_SWIER;
 	 volatile uint32_t EXTI_PR;
 }EXTI_Typedef;
-
+typedef struct {
+  volatile uint32_t NVIC_ISER0;
+	volatile uint32_t NVIC_ISER1;
+	volatile uint32_t NVIC_ISER2;
+	volatile uint32_t NVIC_ICER0;
+	volatile uint32_t NVIC_ICER1;
+	volatile uint32_t NVIC_ICER2;
+	volatile uint32_t NVIC_ISPR0;
+	volatile uint32_t NVIC_ISPR1;
+	volatile uint32_t NVIC_ISPR2;
+	
+}NVIC_Typedef;
 #define EXTI        ((volatile EXTI_Typedef*)(EXTI_ADD_BASE))
+#define NVIC        ((volatile NVIC_Typedef*)(NVIC_ADD_BASE))
 	
 void EXTI_EnableInterrupt(uint8_t line);
 void EXTI_DisableInterrupt(uint8_t line);
-void EXTI_SetMode(uint8_t line, EXTI_MODE mode);
 uint8_t EXTI_GetPending(uint8_t line);
 void EXTI_ClearPending(uint8_t line);
+void EXTI_Init(GPIO_Typedef *port, uint8_t pin, EXTI_MODE mode);
+void NVIC_EXTI0_Enable(void);
+void NVIC_EXTI1_Enable(void);
+void NVIC_EXTI2_Enable(void);
+void NVIC_UART1_Enable(void);
+void EXTI0_IRQHandler(void);
 
 
 
